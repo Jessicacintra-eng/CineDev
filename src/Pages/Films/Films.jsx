d4cb98e91a564a1c81c875e2db720abc8104c363
ViniMocelin <vinemocelin@gmail.com>
2022-08-08 19:48:00 -0300

    arrumando detalhes ainda

--- src/Pages/Films/Films.jsx
+++ src/Pages/Films/Films.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Carousel from '../../Components/Carousel/Carousel'
-import S from "../Films/Films.module.css"
+import s from "./Films.module.css"
+import { BiSearchAlt2, BiCameraMovie } from 'react-icons/bi'
 
 const Films = () => {
   return (
-    <main classname="stars">
-      <section> 
-      <div className='titulo'>
-                <img className="texto" src="https://mcusercontent.com/8d01a411f692a38c571bf7140/images/054194ff-4a77-9887-fddc-82447b278dd3.png" />
-         </div>
-         </section>
-     <div className='carrossel'>
-    <Carousel />
-    </div>
+    <main className={s.paginaFilmes}>
+      <section className={s.brilho}>
+        <div>
+          <img className="texto" src="https://mcusercontent.com/8d01a411f692a38c571bf7140/images/054194ff-4a77-9887-fddc-82447b278dd3.png" />
+        </div>
+      </section>
+      <div class={s.buscarFilme}>
+        <form>
+            <input type="text" placeholder='Buscar um filme' className={s.inputBuscar} />
+          <button type='sumbit' className={s.buttonBuscar}> <BiSearchAlt2/> </button>
+       
+        </form>
+      </div>
+      <div className={s.carrossel}>
+        <Carousel />
+      </div>
     </main>
   )
 }
