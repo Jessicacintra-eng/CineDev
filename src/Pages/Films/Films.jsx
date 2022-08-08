3e755bd42c792ef2a9f868c039283d4ff4331b70
ViniMocelin <vinemocelin@gmail.com>
2022-08-08 20:29:05 -0300

    fundo animado funcionando

--- src/Pages/Films/Films.jsx
+++ src/Pages/Films/Films.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Carousel from '../../Components/Carousel/Carousel'
 import s from "./Films.module.css"
-import { BiSearchAlt2, BiCameraMovie } from 'react-icons/bi'
+import { BiSearchAlt } from 'react-icons/bi'
 
 const Films = () => {
   return (
-    <main className={s.paginaFilmes}>
+    <section className={s.paginaFilmes}>
+      <section className={s.fundoPontilhado}></section>
       <section className={s.brilho}>
         <div>
           <img className="texto" src="https://mcusercontent.com/8d01a411f692a38c571bf7140/images/054194ff-4a77-9887-fddc-82447b278dd3.png" />
@@ -14,14 +15,14 @@ const Films = () => {
       <div class={s.buscarFilme}>
         <form>
             <input type="text" placeholder='Buscar um filme' className={s.inputBuscar} />
-          <button type='sumbit' className={s.buttonBuscar}> <BiSearchAlt2/> </button>
+          <button type='sumbit' className={s.buttonBuscar}> <BiSearchAlt/> </button>
        
         </form>
       </div>
       <div className={s.carrossel}>
         <Carousel />
       </div>
-    </main>
+    </section >
   )
 }
 
